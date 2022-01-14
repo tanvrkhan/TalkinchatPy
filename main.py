cbda5dc2c9d6faa5a71619d896fc66d6bb6e471f
dbh4ck <16207658+dbh4ck@users.noreply.github.com>
2022-01-14 01:54:32 -0800

    .DS_Store banished!

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ BOT_MASTER_ID = "docker"
 GROUP_TO_INIT_JOIN = "american"
 BOT_ID = "botcoder"
 BOT_MASTER = "docker"
-BOT_PWD = "XXXXXXXXXX"
+BOT_PWD = "G0lly~085+16"
 ########## ------- ##########
 
 IMG_TYPE_PNG = "image/png"
